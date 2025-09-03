fba65e53b61f3435feaee3712372d4f5986490b8
alvar <alvar@ALVARO>
2025-09-03 00:17:06 -0300

    Agregar archivos de proyecto.

---
+++ AlumnosBD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace EJ01
+{
+    public class AlumnosDB
+    {
+        private string connectionString =
+          "Data Source=localhost;Initial Catalog=EJ01;Integrated Security=True;";
+        public bool ProbarConexion()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Conexión exitosa a la base de datos.");
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al conectar: " + ex.Message);
+                    return false;
+                }
+            }
+
+        }
+    }
+}
